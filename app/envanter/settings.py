39fa364e474b8083de470e60ad5b11a02ad83576
CodeWizardette <nurterzi1925@icloud.com>
2023-08-17 23:21:31 +0300

    main

--- app/envanter/settings.py
+++ app/envanter/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'envanter'
+    'app.envanter'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
