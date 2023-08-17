00f7e502078c436c2b23613cd4221206fa1e90dc
CodeWizardette <nurterzi1925@icloud.com>
2023-08-17 22:15:52 +0300

    index update

--- envanter/envanter/views.py
+++ envanter/envanter/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Category, Component
 
+
+def index(request):
+    search_model = request.GET.get('search_model')
+    components = Component.objects.all()
+    if search_model:
+        components = components.filter(model__icontains=search_model)
+    return render(request, 'inventory/index.html', {'components': components})
+
 def index(request):
     categories = Category.objects.all()
     components = Component.objects.all()
