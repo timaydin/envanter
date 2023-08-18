80099060ba0b5275365431b4855a05e63bce9b14
CodeWizardette <nurterzi1925@icloud.com>
2023-08-18 09:32:33 +0300

    main

--- envanter/envanter/models.py
+++ envanter/envanter/models.py
@@ -1,5 +1,24 @@
 from django.db import models
+import os 
+from datetime import datetime
 
+class W(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500, null=True, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Ohm(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500, null=True, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
@@ -102,3 +121,23 @@ class PurchaseDetail(models.Model):
     
     def __str__(self):
         return f"Purchase Detail #{self.id}"
+    
+class Datasheet(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    component = models.ForeignKey(Component, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    cost = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    total_cost = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    
+    def __str__(self):
+        return f"Data Sheet #{self.id}"
+    
+class PictureURL(models.Model):
+    id = models.AutoField(primary_key=True)
+    component = models.ForeignKey(Component, on_delete=models.CASCADE)
+    url = models.CharField(max_length=500)
+    
+    def __str__(self):
+        return f"Picture URL #{self.id}"
+    
