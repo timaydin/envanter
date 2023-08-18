af983f0745a92c338cf65659bce2eb04e8ceab83
nt <nurterzi1925@icloud.com>
2023-08-19 00:41:10 +0300

    Update settings.py

--- envanter/envanter/settings.py
+++ envanter/envanter/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g^zt7awo6vy6+dl#bmqhw7mxbz84bdtr-^cj2)f=di8$33q7g3'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
