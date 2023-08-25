b3634c2bf5920f34e10b9c11c7ddffa38e2e314c
nt <nurterzi1925@icloud.com>
2023-08-25 18:43:41 +0300

    Update settings.py

--- envanter/settings.py
+++ envanter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [37.148.211.112]
 
 
 # Application definition
@@ -67,15 +67,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'envanter.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+ #Database https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
