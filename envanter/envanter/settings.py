e788d19182c9c3530003cf9a0af9cdb13d9a96d8
CodeWizardette <nurterzi1925@icloud.com>
2023-08-26 17:23:29 +0300

    main

--- envanter/envanter/settings.py
+++ envanter/envanter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = [37.148.211.112]
+ALLOWED_HOSTS = ['37.148.211.112']
 
 
 # Application definition
